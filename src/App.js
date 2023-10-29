3a329ae477ff8aba50ec02f6a0cc9ab3cffdc581
infinitemax <m.brookman.byrne@gmail.com>
2023-10-29 12:41:00 +0000

    work through the basic Axios get request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Title from './components/Title';
+import Setup from "./examples/1-first-request"
+
+
 function App() {
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
