de53a75186e484c7992084a682eaa529a9bf6e9c
infinitemax <m.brookman.byrne@gmail.com>
2023-10-29 13:52:20 +0000

    work through post request section of the tutorial

--- src/examples/3-post-request.js
+++ src/examples/3-post-request.js
@@ -1,49 +1,68 @@
-import { useState } from 'react';
-import axios from 'axios';
-const url = 'https://course-api.com/axios-tutorial-post';
+import { useState } from "react";
+import axios from "axios";
+const url = "https://course-api.com/axios-tutorial-post";
 
 const PostRequest = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(name, email);
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(name, email);
 
-  return (
-    <section>
-      <h2 className='text-center'>post request</h2>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input
-            type='text'
-            className='form-input'
-            id='name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
-            email
-          </label>
-          <input
-            type='email'
-            className='form-input'
-            id='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <button type='submit' className='btn btn-block'>
-          login
-        </button>
-      </form>
-    </section>
-  );
+        try {
+            const {data} = await axios.post(
+                url,
+                {
+                    name: name,
+                    email: email,
+                },
+                // I've added this header to see if it appears in the network in dev tools, and it does!!!
+                {
+                    headers: {
+                        hello: "hello",
+                    },
+                }
+            );
+            console.log(data.user);
+        } catch (error) {
+            console.log(error.response);
+        }
+    };
+
+    return (
+        <section>
+            <h2 className="text-center">post request</h2>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="form-row">
+                    <label htmlFor="name" className="form-label">
+                        name
+                    </label>
+                    <input
+                        type="text"
+                        className="form-input"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="email" className="form-label">
+                        email
+                    </label>
+                    <input
+                        type="email"
+                        className="form-input"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <button type="submit" className="btn btn-block">
+                    login
+                </button>
+            </form>
+        </section>
+    );
 };
 export default PostRequest;
